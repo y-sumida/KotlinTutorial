431c3445c6362060df9f7b0cbd2e6680cb55dded
Yuki Sumida <y.sumida.0201@gmail.com>
2019-02-07 23:20:01 +0900

    Randomボタンタップで画面遷移する

--- app/src/main/java/com/example/y_sumida/kotolintutorial/MainActivity.kt
+++ app/src/main/java/com/example/y_sumida/kotolintutorial/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.y_sumida.kotolintutorial
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -26,4 +27,9 @@ class MainActivity : AppCompatActivity() {
         showCountTextView.text = count.toString()
     }
 
+    fun randomMe(view: View) {
+        val randomIntent = Intent(this, SecondActivity::class.java)
+
+        startActivity(randomIntent)
+    }
 }
